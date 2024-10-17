1e33784b263b40c88282dffcc48aa96f75a17871
Dr Arjun Anil Kumar <arjunanilk@nitc.ac.in>
2024-10-17 21:44:48 +0530

    correlation updated

--- MS6107E/code_descriptive_statistics.R
+++ MS6107E/code_descriptive_statistics.R
@@ -31,4 +31,112 @@ boxplot(data$crim,outline = FALSE)
 boxplot(data$crim,data$medv,outline = FALSE,names=c("Crime Rate","Price"), main = 'Crime Rate and Price Boxplots')
 
 
+#correlation
+#Pearson correlation is often used for quantitative continuous variables that have a linear relationship
+#Spearman correlation (which is actually similar to Pearson but based on the ranked values for 
+#each variable rather than on the raw data) is often used to evaluate relationships involving at 
+#least one qualitative ordinal variable or two quantitative variables if the link is partially linear
+#Kendall’s tau-b which is computed from the number of concordant and discordant pairs is often used for
+#qualitative ordinal variables
+cor(data$crim,data$medv,method = "spearman")
+cor(data$crim,data$medv,method = "pearson")
+cor(data$crim,data$medv,method = "kendall")
+
+# correlation for all variables
+# correlation for all variables
+round(cor(data),digits = 2)
+
+
+
+
+#scatterplot
+# scatterplot
+library(ggplot2)
+ggplot(data) +
+  aes(x = crim, y = medv) +
+  geom_point(colour = '#0c4c8a') +
+  theme_minimal()+
+  ggtitle('Median House Price and Crime Rate')+
+  xlab('Crime Rate')+
+  ylab('Median House Price')
+
+
+#scatter plot for various combinations
+pairs(data[, c("lstat", "rm", "medv")])
+
+# improved correlation matrix
+library(corrplot)
+corrplot(cor(data[, c("lstat", "rm", "medv")]),
+         method = "number",
+         type = "upper" # show only upper side
+)
+
+#correlogram
+# do not edit
+corrplot2 <- function(data,
+                      method = "pearson",
+                      sig.level = 0.05,
+                      order = "original",
+                      diag = FALSE,
+                      type = "upper",
+                      tl.srt = 90,
+                      number.font = 1,
+                      number.cex = 1,
+                      mar = c(0, 0, 0, 0)) {
+  library(corrplot)
+  data_incomplete <- data
+  data <- data[complete.cases(data), ]
+  mat <- cor(data, method = method)
+  cor.mtest <- function(mat, method) {
+    mat <- as.matrix(mat)
+    n <- ncol(mat)
+    p.mat <- matrix(NA, n, n)
+    diag(p.mat) <- 0
+    for (i in 1:(n - 1)) {
+      for (j in (i + 1):n) {
+        tmp <- cor.test(mat[, i], mat[, j], method = method)
+        p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+      }
+    }
+    colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+    p.mat
+  }
+  p.mat <- cor.mtest(data, method = method)
+  col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+  corrplot(mat,
+           method = "color", col = col(200), number.font = number.font,
+           mar = mar, number.cex = number.cex,
+           type = type, order = order,
+           addCoef.col = "black", # add correlation coefficient
+           tl.col = "black", tl.srt = tl.srt, # rotation of text labels
+           # combine with significance level
+           p.mat = p.mat, sig.level = sig.level, insig = "blank",
+           # hide correlation coefficients on the diagonal
+           diag = diag
+  )
+}
+
+# edit from here
+corrplot2(
+  data = data[, c("indus","lstat", "rm", "medv")],
+  method = "pearson",
+  sig.level = 0.05,
+  order = "original",
+  diag = FALSE,
+  type = "upper",
+  tl.srt = 75
+)
+
+# significance of correlation
+# Pearson correlation test
+test <- cor.test(data$medv, data$rm)
+test
+
+
+
+
+
+
+
+
 
